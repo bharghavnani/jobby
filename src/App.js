9f5730c1dda3aeb9346ae7f4a8ea452fc35ee47e
bharghavchari <bharghavchari537@gmail.com>
2023-02-15 14:04:52 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './component/LoginForm'
+import Home from './component/Home'
+import Jobs from './component/Jobs'
+import JobItemDetails from './component/JobItemDetails'
+import NotFound from './component/NotFound'
+import ProtectedRoute from './component/ProtectedRoute'
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
